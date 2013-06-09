ed757f55887b12188095fc21ee51bc6e37c65880
Nykolas Lima <nykolas.lima@gmail.com>
2013-06-09 18:53:36 -0300

    adding unit test for Data Deserializer with many data fields

--- src/test/java/br/com/provaServer/infrastructure/deserialization/gson/template/DataDeserializerTest.java
+++ src/test/java/br/com/provaServer/infrastructure/deserialization/gson/template/DataDeserializerTest.java
@@ -15,15 +15,32 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
 public class DataDeserializerTest {
+	private DataDeserializer deserializer = new DataDeserializer();
+	
 	@Test
 	public void shouldDeserializeDataWithOneDataField() {
 		String json = "{name: \"nykolas\"}";
 		JsonElement jsonElement = new JsonParser().parse(json);
 		
-		DataDeserializer deserializer = new DataDeserializer();
 		Data data = deserializer.deserialize(jsonElement, mock(Type.class), mock(JsonDeserializationContext.class));
 		
 		assertTrue(data.getValues().containsKey("name"));
 		assertEquals(data.getValues().get("name"), "nykolas");
+	}
+	
+	@Test
+	public void shouldDeserializeDataWithManyFields() {
+		String json = "{name: \"nykolas\", age: \"22\", city: \"sao paulo\"}";
+		JsonElement jsonElement = new JsonParser().parse(json);
+		
+		Data data = deserializer.deserialize(jsonElement, mock(Type.class), mock(JsonDeserializationContext.class));
+		
+		assertTrue(data.getValues().size() == 3);
+		assertTrue(data.getValues().containsKey("name"));
+		assertEquals(data.getValues().get("name"), "nykolas");
+		assertTrue(data.getValues().containsKey("age"));
+		assertEquals(data.getValues().get("age"), "22");
+		assertTrue(data.getValues().containsKey("city"));
+		assertEquals(data.getValues().get("city"), "sao paulo");
 	}
 }
