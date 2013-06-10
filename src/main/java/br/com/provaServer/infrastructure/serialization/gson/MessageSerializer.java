9ad0261eac9b17e0013c0972068ea70ba3025c09
Nykolas Lima <nykolas.lima@gmail.com>
2013-06-10 00:28:14 -0300

    adding Message serializer to deal with I18nMessages used by validations

---
+++ src/main/java/br/com/provaServer/infrastructure/serialization/gson/MessageSerializer.java
@@ -0,0 +1,25 @@
+package br.com.provaServer.infrastructure.serialization.gson;
+
+import java.lang.reflect.Type;
+
+import br.com.caelum.vraptor.ioc.Component;
+import br.com.caelum.vraptor.ioc.RequestScoped;
+import br.com.caelum.vraptor.validator.Message;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+@Component
+@RequestScoped
+public class MessageSerializer implements JsonSerializer<Message> {
+
+	@Override
+	public JsonElement serialize(Message message, Type type, JsonSerializationContext context) {
+		String json = "{category:\"%s\",message:\"%s\"}";
+		
+		return new JsonParser().parse(String.format(json, message.getCategory(), message.getMessage()));
+	}
+
+}
